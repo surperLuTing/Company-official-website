71852b3ac020ed2ec7c9c7487799cb54140def46
高路婷 <825061313@qq.com>
2017-07-29 09:57:12 +0800

    微信小程序之公司官网

---
+++ quickstart/pages/index6/index6.js
@@ -0,0 +1,14 @@
+// pages/index6/index6.js
+Page({
+  data: {
+    markers: [{
+      iconPath: "/images/Icon_position.png",
+      id: 0,
+      latitude: 30.2457600000,
+      longitude: 120.0673800000,
+      width: 30,
+      height: 30
+    }]
+
+  }
+})
